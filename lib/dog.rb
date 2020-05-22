3384372eb7722000966822eb00b4bf11c33cc1ce
Oscar Ore <oscar.ore2011@gmail.com>
2020-05-22 20:34:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,10 @@
 Class Dog
 
-def name=(rover)
-  @rover = rover 
-end 
-
-def name 
-  @rover
-end 
-  
 def bark
   puts "Woof!"
 end 
 
 def sit 
   puts "The Dog is sitting"
-end 
-
+  end 
 end 
